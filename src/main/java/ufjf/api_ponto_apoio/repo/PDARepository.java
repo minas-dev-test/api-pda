90f23346e36f764a14170231748951ea686d4084
Alex Maroco <alexsmaroco@gmail.com>
2018-07-24 13:31:18 -0300

    Adiciona queries por nome, endereco, cep e cidade

--- src/main/java/ufjf/api_ponto_apoio/repo/PDARepository.java
+++ src/main/java/ufjf/api_ponto_apoio/repo/PDARepository.java
@@ -3,14 +3,25 @@ package ufjf.api_ponto_apoio.repo;
 
 
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+
 
 import ufjf.api_ponto_apoio.classes.PDA;
 
-// Fornece algumas queries simples(CRUD)
+// Fornece algumas queries simples
 @Repository
 public interface PDARepository extends MongoRepository<PDA,String> {
 
-    public PDA findByNome(String nome);
+    // (@campo cep) == (@param cep)
+    public List<PDA> findByCEP(String cep);
+
+    // equivalente a | select * from (@repo) where (@campo) like (@param)
+    public List<PDA> findByNomeLike(String nome);
+    public List<PDA> findByCidadeLike(String cidade);
+    public List<PDA> findByBairroLike(String bairro);
+    public List<PDA> findByEnderecoLike(String endereco);
 
 }
